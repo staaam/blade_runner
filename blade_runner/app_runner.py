c141595fdac18bfa8e2224c1ea7d8d6bbef4856d
sxmichael <michael@senexx.com>
2016-08-16 19:29:02 +0100

    actions added

--- blade_runner/app_runner.py
+++ blade_runner/app_runner.py
@@ -58,7 +58,55 @@ class BundleParameter(Parameter):
 
 
 class BaseAction(object):
-    pass
+    def __init__(self):
+        pass
+
+    def apply(self, config):
+        pass
+
+
+class ListAction(BaseAction):
+    def __init__(self, name):
+        super(ListAction, self).__init__()
+        self.name = name
+        self.actions = []
+
+    def add_action(self, action):
+        self.actions.append(action)
+
+    def apply(self, config):
+        for action in self.actions:
+            action.apply(config)
+
+
+class BundleAction(ListAction):
+    def __init__(self, name):
+        super(BundleAction, self).__init__(name)
+        self.actions = []
+
+    def apply(self, config):
+        for action in self.actions:
+            action.apply(config[action.name])
+
+
+class CurlAction(BaseAction):
+    def __init__(self, url, method, data_template):
+        super(CurlAction, self).__init__()
+        self.url = url
+        self.method = method
+        self.data_template = data_template
+
+    def apply(self, config):
+        logger.error("CURL -X{method} {url} -D {data_template}".format(**self.__dict__).format(**config))
+
+
+class CommandAction(BaseAction):
+    def __init__(self, command):
+        super(CommandAction, self).__init__()
+        self.command = command
+
+    def apply(self, config):
+        logger.error(self.command.format(**config))
 
 
 param_factory = {
@@ -66,6 +114,10 @@ param_factory = {
     'list': ListParameter
 }
 
+action_factory = {
+    'curl': CurlAction,
+    'command': CommandAction
+}
 
 def main():
     config_file = "../conf/config.yml"
@@ -74,8 +126,9 @@ def main():
 
     for app, app_conf in config.iteritems():
         app_param = BundleParameter(app)
-        for bundle, bundle_conf in app_conf.iteritems():
-            bundle_param = BundleParameter(bundle)
+        app_action = BundleAction(app)
+        for bundle_name, bundle_conf in app_conf.iteritems():
+            bundle_param = BundleParameter(bundle_name)
             for param_name, param_value in bundle_conf['params'].iteritems():
                 param_type = param_value.pop('type')
                 param_cls = param_factory.get(param_type)
@@ -84,13 +137,22 @@ def main():
                     raise Exception("param type %s not exists" % param_type)
 
                 bundle_param.add_param(param_cls(name=param_name, **param_value))
-
             app_param.add_param(bundle_param)
-        test = list(app_param.apply())
-        print test
 
+            bundle_action = ListAction(bundle_name)
+            for action_value in bundle_conf['actions']:
+                action_type = action_value.pop('type')
+                action_cls = action_factory.get(action_type)
+                if not action_cls:
+                    logger.error("action type %s not exists", action_type)
+                    raise Exception("action type %s not exists" % action_type)
 
-    pass
+                bundle_action.add_action(action_cls(**action_value))
+            app_action.add_action(bundle_action)
+
+        for app_config in app_param.apply():
+            app_action.apply(app_config)
+
 
 if __name__ == "__main__":
     logging.basicConfig()
